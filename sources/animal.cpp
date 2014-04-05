b1421ce7d3c1ada89bbd819336f6b4734801ca4d
valentindu64 <valentindu64@hotmail.fr>
2014-04-05 16:23:32 +0200

    BOUM

--- sources/animal.cpp
+++ sources/animal.cpp
@@ -10,6 +10,29 @@ using namespace std;
 /***************************************************************/
 /***************************************************************/
 
+
+
+
+
+
+
+
+
+// C'est NUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Constructeurs et destructeur
 Animal::Animal():iID(-1), saitVoler(false), saitNager(false), estCarnivore(false), sNom("Nom indéfini") {}
 
